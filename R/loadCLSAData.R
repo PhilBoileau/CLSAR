7f5f067cb247b29b0aa8efa6900d2a3dc6589553
Phil Boileau <pboileau.swim@gmail.com>
2018-04-30 14:35:58 -0400

    Update loadCLSAData function

--- R/loadCLSAData.R
+++ R/loadCLSAData.R
@@ -1,36 +1,44 @@
-#' Load CLSA Data Sets
+#' @title Load CLSA Comprehensive and Tracking Datasets
 #'
-#' Make sure that the csv files containing the data are as follows:  
-#' tracking: tra3_3.csv  
-#' comprehensive: cop3_2.csv  
-#' MCQ TRM: Tracking_30minQv2_Baseline.csv  
-#' MCQ COM: Comprehensive_30minQv2_Baseline.csv  
+#' @description
+#' Load the most recent comprehensive and tracking data sets. Note that
+#' the .csv files must already be saved on your computer before using this
+#' function. Make sure that the csv files containing the data are named 
+#' as follows:
+#' \itemize{
+#'   \item comprehensive: cop3_2.csv
+#'   \item tracking: tra3_3.csv  
+#' }
+#' 
+#' @param path The path to the folder containging the comprehensive and
+#' tracking .csv files  
 #'
-#' @param path The path to the folder containging the data files  
-#'
-#' @return A list of 4 CLSA data sets, in the following order:
-#' tracking, comprehensive, MCQ tracking and MCQ comprehensive
+#' @return A list of the comprehensive and tracking CLSA datasets in the following order:
+#' \enumerate{
+#'   \item comprehensive
+#'   \item tracking
+#' }
 #' 
 #' @import utils
 #' 
 #' @export
 #'
+#' @example 
+#' \dontrun{
+#' loadCLSAData(path = "C:/Users/Documents")
+#' }
 loadCLSAData <- function(path){
   
   # load the data sets
   tra3.3 <- read.csv(paste(path, "/tra3_3.csv", sep = ""), na.string = "")
   cop3.2 <- read.csv(paste(path, "/cop3_2.csv", sep = ""), na.string = "")
-  mcq.tra <- read.csv(paste(path, "/Tracking_30minQv2_Baseline.csv", sep = ""), na.string = "")
-  mcq.cop <- read.csv(paste(path, "/Comprehensive_30minQv2_Baseline.csv", sep = ""), na.string = "")
   
   # order the data by entitiy ID
   tra3.3 <- tra3.3[order(tra3.3$entity_id),]
   cop3.2 <- cop3.2[order(cop3.2$entity_id),]
-  mcq.tra <- mcq.tra[order(mcq.tra$entity_id),]
-  mcq.cop <- mcq.cop[order(mcq.cop$entity_id),]
   
   # return the list of data
-  dataList <- list(tra3.3, cop3.2, mcq.tra, mcq.cop) 
+  dataList <- list(cop3.2, tra3.3) 
   return(dataList)
 
 }
